45a7cdeeacc99c3f1df83824bf15ee803dff34be
Suryakant Thombre <suryathombre000@gmail.com>
2024-01-25 00:32:50 +0530

    basic routes with express and dotenv added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello backend");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("suryathombare");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login first</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});

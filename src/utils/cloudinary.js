2965212d67e1a2b82c35bb808eec930df010ffa6
Suryakant Thombre <suryathombre000@gmail.com>
2024-01-29 00:32:19 +0530

    cloudinary uploader

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,30 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath, remoteFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File is uploaded to cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the temporary file as the cloudinary upload will fail
+
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
+
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );

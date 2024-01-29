4dd9e784ff2b38471892c64c04677e4fb477a894
Suryakant Thombre <suryathombre000@gmail.com>
2024-01-30 01:38:53 +0530

    register user controller

--- src/model/user.models.js
+++ src/model/user.models.js
@@ -49,9 +49,9 @@ const useSchema = new Schema(
   { timestamps: true }
 );
 
-useSchema.pre("save", function (next) {
+useSchema.pre("save", async function (next) {
   if (!this.isModified("password")) next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
